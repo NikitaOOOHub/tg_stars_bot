8d0e1c0002e3f600659ccbf5694d0ef4d793a202
NikitaOOOHub <kukunela0@gmail.com>
2026-07-27 18:48:47 +0300

    Delete prices.py

--- prices.py
+++
@@ -1,7 +0,0 @@
-def calculate_stars_price(quantity: int) -> float:
-    if quantity <= 300:
-        return round(quantity * 1.30, 2)
-    elif quantity <= 500:
-        return round(quantity * 1.25, 2)
-    else:
-        return round(quantity * 1.22, 2)
