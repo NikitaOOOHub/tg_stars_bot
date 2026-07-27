d6be26cb404ab273ceaf124944f182539c7207a6
NikitaOOOHub <kukunela0@gmail.com>
2026-07-27 20:29:19 +0300

    Fix FragmentSender class initialization method

--- services/fragment_sender.py
+++ services/fragment_sender.py
@@ -14,7 +14,7 @@ def fix_base64_padding(b64_string: str) -> str:
     return b64_string
 
 class FragmentSender:
-    def _init_(self, config: Config, bot: Bot):
+    def __init__(self, config: Config, bot: Bot):
         self.config = config
         self.bot = bot
         self.url = "https://fragment.com/api"
